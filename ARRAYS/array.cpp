0968b419455be28aa38a5ea0177265d92b92cbcf
ritwik-singh <31265500+ritwik-singh@users.noreply.github.com>
2019-06-06 04:09:15 +0530

    Update array.cpp

--- ARRAYS/array.cpp
+++ ARRAYS/array.cpp
@@ -1,1 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+void input(int arr [],int n)
+{
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+}
+
+void print(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<endl;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    input(a,n);
+    reverse(a,a+n);
+    print(a,n);
+    
+    return 0;
+}
