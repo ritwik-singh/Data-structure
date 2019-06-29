73b0b6a4ec12910e41fde20ba4a198d466e853b7
ritwik-singh <31265500+ritwik-singh@users.noreply.github.com>
2019-06-30 00:04:13 +0530

    Update functions.hpp

--- linked lists/functions.hpp
+++ linked lists/functions.hpp
@@ -1,1 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+class Node{
+	public:
+		int data;
+		Node *link;
+
+		Node(int info){
+			this->data = info;
+			this->next = nullptr;
+		}
+
+};
+
+class LinkedList{
+	public:
+		Node *head;
+		Node *tail;
+
+		LinkedList(){
+			this->head = nullptr;
+			this->tail = nullptr;
+		}
+
+		void insert_node(int info){
+			Node* node = new Node(info);
+			if(!this->data)
+				this->head = node;
+			else
+				this->tail->next = node;
+
+			this->tail = node;		 
+		}
+}
